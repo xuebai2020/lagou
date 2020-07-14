0979e933658c4c9cb366f4fdfaf0496015f81532
xuebai2020 <1248500096@qq.com>
2020-07-14 08:36:39 +0800

    study

--- python实战2-面向对象/实战-bicycle.py
+++ python实战2-面向对象/实战-bicycle.py
@@ -1,7 +1,9 @@
 '''
-写一个bicycle（自行车）类，有run（骑行）方法，调用时现实骑行里程数km（骑行里程为传入的数字）
+写一个bicycle（自行车）类，有run（骑行）方法，调用时显示骑行里程数km（骑行里程为传入的数字）
 再写一个电动自行车ebicycle继承bicycle，添加电池电量battery_level属性通过参数传入，同时有两个方法：
     1.fill_charge(vol)用来充电，vol为电量
     2.run（km）方法用于骑行，每骑行10km消耗电量1度，当电量耗尽时调用bicycle的run方法骑行
 通过传入的骑行里程数，显示骑行结果（就是当电量耗尽，需要你真正骑的里程数）
 '''
+class Bicycle():
+    def run(self):
